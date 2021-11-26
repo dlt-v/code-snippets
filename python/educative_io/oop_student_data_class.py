23b9315f7607c36b002a7c1b15beaf78c576aa48
Tomek Michalski <tmm.michalski@gmail.com>
2021-11-26 11:27:32 +0100

    Complete encapsulation part of the course

---
+++ python/educative_io/oop_student_data_class.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self):
+        self.__name = None
+        self.__rollNumber = None
+
+    def getName(self):
+        return self.__name
+
+    def setName(self, name):
+        self.__name = name
+
+    def getRollNumber(self):
+        return self.__rollNumber
+
+    def setRollNumber(self, rollNumber):
+        self.__rollNumber = rollNumber

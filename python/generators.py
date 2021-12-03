9c51c35c06a7eb4148781cd70cf57e11ef53b22c
Tomek Michalski <tmm.michalski@gmail.com>
2021-12-03 12:57:20 +0100

    refactor generators.py

--- python/generators.py
+++ python/generators.py
@@ -29,24 +29,22 @@ def check_generators(alpha, p):
         print(f'{i}\t{primes[i]}\t{alpha} * ({p - 1}/{primes[i]}) % {p} {equals} 1\t{result}')
 
         if not result:
-            print(f'{alpha} is not a generator number.')
+            print(f'{alpha} is not a generator number.\n')
             break
-    # gen_number = (primes[0]**(primes[0] - 1)) * (primes[0] - 1) * (primes[1]**(primes[1] - 1)) * (primes[1] - 1)
-    # print(f'Ø({p - 1}) = {primes[0]**(primes[0] - 1)} * {primes[0] - 1} * {primes[1]**(primes[1] - 1)} * {primes[1] - 1} = {gen_number}')
 
-
-# check_generators(4, 71)
-
+    get_gen_number(p - 1)
 
 def get_gen_number(number):
-    primes = list(set(prime_factors(number)))
     list_of_primes = prime_factors(number)
+    primes = list(set(list_of_primes))
     result = 1
     e = 0
     for prime in primes:
         result *= prime ** (list_of_primes.count(prime) - 1) * (prime - 1)
         print(f'{prime} ^ ({list_of_primes.count(prime)} - 1) * {prime} - 1')
         e += 1
-    print('=', result)
+    print('Ø =', result)
 
-get_gen_number(70)
+check_generators(4, 71)
+
+

9bcacd8e074ab05efeea7702aa39526faa819d6f
Tomek Michalski <tmm.michalski@gmail.com>
2022-03-11 14:47:11 +0100

    add padding to mode_power

--- python/mod_power.py
+++ python/mod_power.py
@@ -1,28 +1,32 @@
-def mod_power(a, base, n): # base, power, mod
+def format(x):
+    return str(x).rjust(5)
+
+
+def mod_power(a, base, n):  # base, power, mod
     t = base
-    
+
     x = 1
     bin_t = [int(i) for i in list('{0:0b}'.format(t))]
     bin_t.reverse()
-    print('i\tx(i)\ta(i)\tt(i)')
+    print('    i\tx(i)\ta(i)\tt(i)')
 
     i = 0
     while i < len(bin_t):
-        print(f'{i}\t{x}\t{a}\t{bin_t[i]}')
+        print(
+            f'{format(i)}\t{format(x)}\t{format(a)}\t{format(bin_t[i])}')
         i += 1
 
         if bin_t[i - 1] == 1:
             x = (x * a) % n
-        
+
         if i == len(bin_t):
             break
 
         a = (a * a) % n
-    
-    print(f'{i}\t{x}\t{a}\t-\n')
+
+    print(f'{format(i)}\t{format(x)}\t{format(a)}\t{"-".rjust(5)}\n')
 
     print(f'Answer is: {x}')
 
 
-
-mod_power(255, 521, 314) # base, power, mod
+mod_power(255, 521, 314)  # base, power, mod
